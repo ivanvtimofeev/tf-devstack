3f74996c523e6398685444af792e0499a7c1b4c4
Ivan Timofeev <ivanv.timofeev@gmail.com>
2021-03-11 13:42:42 +0300

    More changes

--- openshift/providers/vexx/install_openshift.sh
+++ openshift/providers/vexx/install_openshift.sh
@@ -368,8 +368,8 @@ cat <<EOF > $OPENSHIFT_INSTALL_DIR/$INFRA_ID-bootstrap-ignition.json
     "storage": {
     "files": [{
       "path": "/etc/NetworkManager/conf.d/ens3.conf ",
-      "mode": 0644,
-      "contents": { "source": "data:,%5Bglobal-dns-domain-%2A%5D%0D%0Aservers%3D${default_dns}" }
+      "mode": 420,
+      "contents": { "source": "data:,%5Bglobal-dns-domain-%2A%5D%0Aservers%3D${default_dns}" }
     }]
   }
   },
