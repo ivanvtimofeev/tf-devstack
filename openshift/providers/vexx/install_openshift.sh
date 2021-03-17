2b878c019170c4fe68045fc3df66c5f3ede27ae1
Ivan Timofeev <ivanv.timofeev@gmail.com>
2021-03-17 17:15:39 +0300

    fix an error

--- openshift/providers/vexx/install_openshift.sh
+++ openshift/providers/vexx/install_openshift.sh
@@ -495,6 +495,7 @@ for index in $(seq 0 2); do
     python3 -c "import base64, json, sys;
 ignition = json.load(sys.stdin);
 storage = ignition.get('storage', {});
+files = storage.get('files', []);
 files.append({'path': '/etc/hostname', 'mode': 420, 'contents': {'source': 'data:text/plain;charset=utf-8;base64,' + base64.standard_b64encode(b'$MASTER_HOSTNAME').decode().strip(), 'verification': {}}, 'filesystem': 'root'});
 storage['files'] = files;
 ignition['storage'] = storage
